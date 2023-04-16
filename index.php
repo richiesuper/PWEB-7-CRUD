dd40a61f272f6be61a4b9f14fd2ccb2bd968e4a9
richiesuper <richie9963@gmail.com>
2023-04-17 00:49:10 +0700

    feat: add index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>SMK Coding - Pendaftaran Siswa Baru</title>
+		<link rel="stylesheet" href="style.css">
+		<style>
+		body {
+			text-align: center;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="title">Pendaftaran Siswa Baru</div>
+		<div class="institution">SMK Coding</div>
+		<br>
+		<nav class="menu">
+			<div class="menu-text">Menu</div>
+			<br>
+			<a href="form-daftar.php">Daftar Baru</a>
+			<br>
+			<a href="list-siswa.php">Pendaftar</a>
+		</nav>
+	</body>
+</html>
